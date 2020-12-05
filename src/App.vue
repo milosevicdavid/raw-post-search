ef0c375d7c5a011ec4b708b4551bf0d1835f6cf0
David <milosevicdavid@gmail.com>
2020-12-05 14:00:47 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <br />
+
+      <input
+        @change.prevent="submitTask"
+        v-model="input"
+        type="text"
+        placeholder="Insert task"
+        list="posts"
+        name="posts"
+      />
+      <datalist id="posts">
+        <option id="option-class" v-for="(item, index) in endimion" :key="index">
+            {{ item.title }}
+      </option
+        >
+      </datalist>
+   
+       
+
+      <div v-for="(item, id) in list" :key="id">
+        <Card :item="item" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+
+  data() {
+    return {
+      endimion: [],
+      endimionFive: [],
+      input: "",
+      list: [],
+    };
+  },
+
+  created: function() {
+    return axios
+      .get("https://jsonplaceholder.typicode.com/posts?q=lorem")
+      .then((res) => {
+        this.endimion = res.data;
+      });
+  },
+
+  methods: {
+    submitTask() {
+      if (this.input != "") {
+        this.list = [];
+        this.endimion.map((person) => {
+          if (
+            person.title.toLowerCase().includes(this.input) ||
+            person.title.toUpperCase().includes(this.input) ||
+            person.title.includes(this.input) ||
+            person.body.toLowerCase().includes(this.input) ||
+            person.body.toUpperCase().includes(this.input) ||
+            person.body.includes(this.input)
+          ) {
+            this.list.push(person);
+          }
+        });
+      }
+      this.input = "";
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Card.vue
@@ -0,0 +1,58 @@
+<template>
+  <div  class="card">
+    <div class="card-wrapper">
+      <span class="card-title"
+        ><h1>{{ item.title }}</h1></span
+      >
+      <span class="card-body">{{ `${item.body}.` }}</span>
+    </div>
+  </div>
+  </template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style lang="scss">
+
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &-wrapper {
+    width: 50vw;
+    margin-top: 50px;
+
+    @media (max-width: 576px) {
+      width: 80vw;
+    }
+  }
+
+  &-title {
+    display: block;
+    text-transform: capitalize;
+    background-color: #69c28e;
+    padding: 20px;
+    color: #fff;
+    border-radius: 20px 20px 0 0;
+  }
+
+  &-body {
+    display: block;
+    background-color: #6fce96;
+    padding: 20px;
+    border-radius: 0 0 20px 20px;
+    font-weight: 500;
+    color: #fff;
+    font-size: 16px;
+
+    &::first-letter {
+      text-transform: capitalize;
+    }
+  }
+}
+</style>
+
